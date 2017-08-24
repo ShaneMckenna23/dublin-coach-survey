ffb17a04fcec870baa2d73cd45206f462f583585
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2017-08-24 16:31:23 +0100

    Fixed failing test, removed result page
    Result page will be seperate project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route,BrowserRouter } from 'react-router-dom'
 import WelcomePage from './components/WelcomePage/WelcomePage.js'
 import SurveyPage from './components/SurveyPage/SurveyPage.js'
-import ResultsPage from './components/ResultsPage/ResultsPage.js'
 
 
 const App = () => (
   <main>
-    <Switch>
-      <Route exact path='/' component={WelcomePage}/>
-      <Route path='/survey' component={SurveyPage}/>
-      <Route path='/results' component={ResultsPage}/>
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={WelcomePage}/>
+        <Route path='/survey' component={SurveyPage}/>
+      </Switch>
+    </BrowserRouter>
   </main>
 )
 
--- src/components/SurveyPage/SurveyPage.js
+++ src/components/SurveyPage/SurveyPage.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 import Iframe from 'react-iframe'
-import {Link} from 'react-router-dom'
+import {Link, Switch, Route } from 'react-router-dom'
+import {Button} from 'react-bootstrap'
+import Sidebar from '../Sidebar/Sidebar'
+import WelcomePage from '../WelcomePage/WelcomePage'
 
 class SurveyPage extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+      
+        this.state = {
+        isVisible: false,
+      };
+    }
+    
+    updateModal(isVisible) {
+        this.state.isVisible = isVisible;
+      this.forceUpdate();
+    }
     render () {
         return (
             <div>
-                <h1>Survey Page</h1>
+                <div className='Sidebar-demo'>
+                    <Button onClick={ () => this.updateModal(true) }>Display Modal Dialog</Button>
+                    <Sidebar side='left' isVisible={ this.state.isVisible } onHide={ () => this.updateModal(false) }>
+                        <Switch>
+                            <Route exact path='/survey' component={WelcomePage}/>
+                            <Route exact path='/survey/1' component={WelcomePage}/>
+                        </Switch>
+                    </Sidebar>
+                </div>
                 <Iframe url="http://www.dublincoach.ie"
                     width="450px"
                     height="450px"
