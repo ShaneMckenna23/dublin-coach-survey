dca4c676599ae7bbd58597d9330595f5aa43cb65
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2017-08-25 17:10:06 +0100

    Added terrible route transitions, will mess around with css later

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route,BrowserRouter } from 'react-router-dom'
+import { RouteTransition } from 'react-router-transition';
 import WelcomePage from './components/WelcomePage/WelcomePage.js'
 import SurveyInfo from './components/SurveyInfo/SurveyInfo.js'
 import TaskOne from './components/TaskOne/TaskOne.js'
@@ -7,14 +8,28 @@ import TaskOne from './components/TaskOne/TaskOne.js'
 const App = () => (
   <main>
     <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={WelcomePage}/>
-        <Route path='/survey' component={SurveyInfo}/>
-        <Route path='/task/1' component={TaskOne}/>     
-      </Switch>
+      <Route render={({location, history, match}) => {
+        return (
+          <RouteTransition 
+            className="animated-wrapper"
+            pathname={location.pathname}
+            atEnter={{ opacity: 0, height: "100px" }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+          >
+            <Switch key={location.key} location={location}>
+              <Route exact path='/' component={WelcomePage}/>
+              <Route path='/survey' component={SurveyInfo}/>
+              <Route path='/task/1' component={TaskOne}/> 
+            </Switch>
+          </RouteTransition>
+        );
+      }} />
     </BrowserRouter>
   </main>
 )
 
 export default App
+
+
 
